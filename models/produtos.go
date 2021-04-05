b38ccc85a851822dee8935e80fb0b7211598c4c5
Caroline das Mercês <carolinedasmerces@gmail.com>
2021-04-05 18:37:39 -0300

    adds db and models packages, go mod and sum

---
+++ models/produtos.go
@@ -0,0 +1,43 @@
+package models
+
+import "github.com/carolinemerces/lojaAlura/db"
+
+type Produto struct {
+	Id         int
+	Nome       string
+	Descricao  string
+	Preco      float64
+	Quantidade int
+}
+
+func BuscaTodosOsProdutos() []Produto {
+	db := db.ConectaComBancoDeDados
+	selectDeTodosOsProdutos, err := db.Query("select * from produtos")
+	if err != nil {
+		panic(err.Error())
+	}
+
+	produto := Produto{}
+	produtos := []Produto{}
+
+	for selectDeTodosOsProdutos.Next() {
+		var id, quantidade int
+		var nome, descricao string
+		var preco float64
+
+		err = selectDeTodosOsProdutos.Scan(&id, &nome, &descricao, &preco, &quantidade)
+		if err != nil {
+			panic(err.Error())
+		}
+
+		produto.Nome = nome
+		produto.Descricao = descricao
+		produto.Preco = preco
+		produto.Quantidade = quantidade
+
+		produtos = append(produtos, produto)
+	}
+
+	defer db.Close()
+	return produtos
+}
